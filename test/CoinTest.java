3f0e82952c0c62215719f91f54d388589fb49898
compwron <wordymusic@gmail.com>
2013-02-13 00:28:44 -0600

    .idea files are differnt everywhere; remove from repo?

---
+++ test/CoinTest.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Thoughtworker
+ * Date: 2/13/13
+ * Time: 12:23 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class CoinTest {
+}
